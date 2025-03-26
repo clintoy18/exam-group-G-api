a81f33065b3108e67a27c353853a8131dc7f5571
TT402@ccs.local <TT402@ccs.local>
2025-03-26 18:08:47 +0800

    try conflict

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const examGroupRoute = require('./routes/exam-group-g');
 // Use routes for '/exam-group-g'
 app.use('/exam-group-g', examGroupRoute);
 app.use('/exams', examGroupRoute);
-
+/asdas
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
