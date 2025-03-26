7c90a6d92de9691a1680acb2a4154a6d046c75c8
TT402@ccs.local <TT402@ccs.local>
2025-03-26 18:07:32 +0800

    create conflict

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const examGroupRoute = require('./routes/exam-group-g');
 
-// Middleware to parse JSON
-app.use(express.json());
+// Use routes for '/exam-group-g '
+app.use('/exam-group-g', examGroupRoute);
+app.use('/exams', examGroupRoute);
 
-// Use the routes
-app.use('/', examGroupRoute);
-
+//conflicts
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
