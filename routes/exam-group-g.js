b6c54195f4cf879a764948bfe5147ed5bfc3662b
TT402@ccs.local <TT402@ccs.local>
2025-03-26 17:25:36 +0800

    Added member 1 route

--- routes/exam-group-g.js
+++ routes/exam-group-g.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+// General route for '/'
 router.get('/', (req, res) => {
     res.json({ message: "Group-G API" });
+});
+
+// Route for '/exams'
+router.get('/exams', (req, res) => {
+    res.json({ exams: ["Exam 1", "Exam 2", "Exam 3"] });
 });
 
 module.exports = router;
