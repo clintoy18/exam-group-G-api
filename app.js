558f26b44793eecd1022fa4ae873299a48573966
TT402@ccs.local <TT402@ccs.local>
2025-03-26 18:36:41 +0800

    resolve conflict in main

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const app = express();
-const examGroupRoute = require('./routes/exam-group-g');
+const examGroupRoute = require('./routes/exam-group-routes');
 
-<<<<<<< HEAD
-// Use routes for '/exam-group-g '
-=======
 // Middleware to parse JSON
 app.use(express.json());
 //try merge with main 
 
 // Use the routes
->>>>>>> eaf8a56b0df41cecd015e1c617ed48df078a1cbe
 app.use('/', examGroupRoute);
 //try a conflict
-
 
 const PORT = 3000;
 app.listen(PORT, () => {
